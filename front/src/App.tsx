30ebdd65b6111ab616e0079546fca5118e618144
Beka Sturua <beqasturua3@gmail.com>
2022-11-16 11:09:44 +0400

    add Add User Modal

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,9 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { User } from "../types";
 
+import React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
 import DataTable from "react-data-table-component";
 
 const columns = [
@@ -33,6 +36,10 @@ const columns = [
 ];
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   const [users, setUsers] = useState<Array<User>>([]);
 
   useEffect(() => {
@@ -46,7 +53,40 @@ function App() {
       });
   }, []);
 
-  return <DataTable columns={columns} data={users} />;
+  return (
+    <div>
+      <div>
+        <Button color="danger" onClick={toggle}>
+          Add User
+        </Button>
+        <Modal isOpen={modal} fade={false} toggle={toggle}>
+          <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            <input type="text" placeholder="Name..." />
+            <input type="text" placeholder="Email..." />
+            <input type="text" placeholder="City..." />
+            <input type="text" placeholder="Street..." />
+            <input type="text" placeholder="Phone..." />
+            <select>
+              <option value="Male">Male</option>
+              <option value="Famale">Famale</option>
+            </select>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={toggle}>
+              Add
+            </Button>
+            <Button color="secondary" onClick={toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+      <div>
+        <DataTable columns={columns} data={users} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
